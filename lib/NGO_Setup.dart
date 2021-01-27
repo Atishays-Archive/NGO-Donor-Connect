c783a4abc0e91c8c793c3a5392cb15018d4a7da1
ATISHAY JAIN <atishayj2202@gmail.com>
2021-01-28 03:14:28 +0530

    Main Work

--- lib/NGO_Setup.dart
+++ lib/NGO_Setup.dart
@@ -1,12 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:ngo_donor_connect/Donor_Home.dart';
-import 'package:ngo_donor_connect/MainPage.dart';
+import 'package:ngo_donor_connect/NGO_Home.dart';
 import 'package:ngo_donor_connect/SignIn.dart';
-import 'package:ngo_donor_connect/StatementDonor.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'StatementNGO.dart';
 
 class MyNGO_Set extends StatefulWidget {
   MyNGO_Set({Key key}) : super(key: key);
@@ -36,37 +35,12 @@ class _MyNGO_Set extends State<MyNGO_Set> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        backgroundColor: Colors.white,
-        title: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              new RaisedButton(
-                child: new Text("Go Back"),
-                onPressed: () async {
-                  var mypos = await Geolocator.getCurrentPosition(
-                      desiredAccuracy: LocationAccuracy.high);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MyMainPage(
-                              lat: mypos.latitude,
-                              long: mypos.longitude,
-                            )),
-                  );
-                },
-              ),
-            ]),
-        automaticallyImplyLeading: false,
-      ),
       body: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Padding(padding: EdgeInsets.only(top: 20.0)),
+            Padding(padding: EdgeInsets.only(top: 50.0)),
             new Text(
               err,
               textAlign: TextAlign.center,
@@ -80,7 +54,7 @@ class _MyNGO_Set extends State<MyNGO_Set> {
               "Description :",
               textAlign: TextAlign.center,
               style: new TextStyle(
-                  fontSize: 60,
+                  fontSize: 40,
                   fontWeight: FontWeight.w200,
                   fontFamily: "Roboto"),
             ),
@@ -91,16 +65,17 @@ class _MyNGO_Set extends State<MyNGO_Set> {
                 }
                 return null;
               },
-              keyboardType: TextInputType.number,
-              inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+              keyboardType: TextInputType.multiline,
+              minLines: 2,
+              maxLines: 2,
               controller: _id,
               style: new TextStyle(
                   color: const Color(0xFFffffff),
                   fontWeight: FontWeight.w200,
                   fontFamily: "Roboto"),
               decoration: new InputDecoration(
-                hintText: "Amount to Donate?",
-                labelText: "Amount",
+                hintText: "Description",
+                labelText: "Description",
                 enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(8)),
                     borderSide: BorderSide(width: 4, color: Colors.green)),
@@ -111,6 +86,56 @@ class _MyNGO_Set extends State<MyNGO_Set> {
                     borderRadius: BorderRadius.all(Radius.circular(8)),
                     borderSide: BorderSide(width: 4, color: Colors.white)),
               ),
+            ),
+            new RaisedButton(
+              onPressed: () {
+                print("hello");
+              },
+              child: new Text("Update Description"),
+            ),
+            Padding(padding: EdgeInsets.only(top: 20.0)),
+            new Text(
+              "Address :",
+              textAlign: TextAlign.center,
+              style: new TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w200,
+                  fontFamily: "Roboto"),
+            ),
+            new TextFormField(
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              keyboardType: TextInputType.multiline,
+              minLines: 2,
+              maxLines: 2,
+              controller: _id,
+              style: new TextStyle(
+                  color: const Color(0xFFffffff),
+                  fontWeight: FontWeight.w200,
+                  fontFamily: "Roboto"),
+              decoration: new InputDecoration(
+                hintText: "Address",
+                labelText: "Address",
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    borderSide: BorderSide(width: 4, color: Colors.green)),
+                errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    borderSide: BorderSide(width: 4, color: Colors.red)),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    borderSide: BorderSide(width: 4, color: Colors.white)),
+              ),
+            ),
+            new RaisedButton(
+              onPressed: () {
+                print("hello");
+              },
+              child: new Text("Update Address"),
             ),
           ]),
       bottomNavigationBar: BottomAppBar(
@@ -124,23 +149,17 @@ class _MyNGO_Set extends State<MyNGO_Set> {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => MyDonorH()),
+                  MaterialPageRoute(builder: (context) => MyNGOh()),
                 );
               },
             ),
             IconButton(
-              icon: Icon(Icons.search),
+              icon: Icon(Icons.request_page_sharp),
               disabledColor: Colors.green,
-              onPressed: () async {
-                var mypos = await Geolocator.getCurrentPosition(
-                    desiredAccuracy: LocationAccuracy.high);
+              onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(
-                      builder: (context) => MyMainPage(
-                            lat: mypos.latitude,
-                            long: mypos.longitude,
-                          )),
+                  MaterialPageRoute(builder: (context) => MyNGOStatement()),
                 );
               },
             ),
@@ -148,17 +167,6 @@ class _MyNGO_Set extends State<MyNGO_Set> {
               icon: Icon(Icons.settings),
               disabledColor: Colors.green,
               onPressed: null,
-            ),
-            IconButton(
-              icon: Icon(Icons.request_page_sharp),
-              onPressed: () async {
-                FirebaseAuth.instance.signOut().then((value) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MyDStatement()),
-                  );
-                });
-              },
             ),
             IconButton(
               icon: Icon(Icons.exit_to_app),
